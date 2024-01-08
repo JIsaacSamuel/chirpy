baff0b61457870865babcb8c00266005849d1ac4
JIsaacSamuel <isij2018@gmail.com>
2024-01-08 22:17:53 +0530

    JSON parsing added

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -41,6 +45,7 @@ func apiHandler(cfg *apiConfig) http.Handler {
 	r.HandleFunc("/reset", cfg.resetMetric)
 	r.Get("/metrics", cfg.printIt)
 	r.Get("/healthz", responder)
+	r.Post("/validate_chirp", validateChirp)
 	return r
 }
 
@@ -68,6 +73,81 @@ func responder(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func cleanBody(message string) (cleanedmessage string) {
+	array := strings.Split(message, " ")
+	for i := 0; i < len(array); i++ {
+		temp := array[i]
+		temp = strings.ToLower(temp)
+		if temp == "kerfuffle" || temp == "sharbert" || temp == "fornax" {
+			temp = "****"
+			array[i] = temp
+		}
+	}
+	return strings.Join(array, " ")
+}
+
+func validateChirp(w http.ResponseWriter, r *http.Request) {
+	type incoming struct {
+		Body string `json:"body"`
+	}
+	type undesired struct {
+		Error string `json:"error"`
+	}
+	type desired struct {
+		Msg string `json:"cleaned_body"`
+	}
+
+	respBody := desired{}
+
+	urespBody := undesired{}
+
+	decoder := json.NewDecoder(r.Body)
+	params := incoming{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	if utf8.RuneCountInString(params.Body) <= 140 {
+		respBody.Msg = cleanBody(params.Body)
+		dat, err := json.Marshal(respBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(dat)
+		return
+	} else if utf8.RuneCountInString(params.Body) > 140 {
+		urespBody.Error = "Chirp is too long"
+		dat, err := json.Marshal(urespBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(dat)
+		return
+	}
+	urespBody.Error = "Something went wrong"
+	dat, err := json.Marshal(urespBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(500)
+	w.Write(dat)
+	return
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
